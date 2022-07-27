97b34fa0eb3c92c16182b26053af68dc3c082e18
Kevin-ramos <55715422+Kevin-ramos@users.noreply.github.com>
2022-07-27 16:50:50 -0500

    arreglo del direction

--- app/src/main/java/com/gomezjair/cazarpatosgmezjair/RankingActivity.kt
+++ app/src/main/java/com/gomezjair/cazarpatosgmezjair/RankingActivity.kt
@@ -1,9 +1,17 @@
 package com.gomezjair.cazarpatosgmezjair
 
+import android.app.DownloadManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+/*
 
 class RankingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,5 +29,38 @@ class RankingActivity : AppCompatActivity() {
         recyclerViewRanking.adapter = RankingAdapter(jugadores);
         recyclerViewRanking.setHasFixedSize(true);
 
+    }
+}*/
+class RankingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ranking)
+        //Codigo anterior comentado ...
+        consultarPuntajeJugadores()
+    }
+    fun consultarPuntajeJugadores() {
+        val db = Firebase.firestore
+        db.collection("ranking")
+            .orderBy("patosCazados", Query.Direction.DESCENDING)
+            .get()
+            .addOnSuccessListener { result ->
+                Log.d(EXTRA_LOGIN, "Success getting documents")
+                var jugadores = ArrayList<Jugador>()
+                for (document in result) {
+                    val jugador = document.toObject(Jugador::class.java)
+                    //val jugador = document.toObject<Jugador>()
+                    jugadores.add(jugador)
+                }
+                //Poblar en RecyclerView información usando mi adaptador
+                val recyclerViewRanking: RecyclerView = findViewById(R.id.recyclerViewRanking);
+                recyclerViewRanking.layoutManager = LinearLayoutManager(this);
+                recyclerViewRanking.adapter = RankingAdapter(jugadores);
+                recyclerViewRanking.setHasFixedSize(true);
+            }
+            .addOnFailureListener { exception ->
+                Log.w(EXTRA_LOGIN, "Error getting documents.", exception)
+                Toast.makeText(this, "Error al obtener datos de jugadores", Toast.LENGTH_LONG)
+                    .show()
+            }
     }
 }
